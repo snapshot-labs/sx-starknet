3a35dc936299ce91f318c0281866fe57bcfb2136
Orland0x <37511817+Orland0x@users.noreply.github.com>
2022-02-02 10:45:57 +0000

    Delete SnapshotXZodiac directory

--- SnapshotXZodiac/contracts/deps.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity >=0.8.4;
-
-import "@gnosis.pm/safe-contracts/contracts/GnosisSafeL2.sol";
-import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol";
-import "@gnosis.pm/zodiac/contracts/factory/ModuleProxyFactory.sol";
-
