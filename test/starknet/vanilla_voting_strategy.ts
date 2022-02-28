cae2d37683245ee4eee1c33b3307004a8b1641d5
Orland0x <37511817+Orland0x@users.noreply.github.com>
2022-02-28 18:30:21 +0000

    Update vanilla_voting_strategy.ts

--- test/starknet/vanilla_voting_strategy.ts
+++ test/starknet/vanilla_voting_strategy.ts
@@ -14,7 +14,6 @@ async function setup() {
 describe('Snapshot X L1 Proposal Executor:', () => {
     it('The module should return the number of transactions in a proposal', async () => {
         const {vanillaVotingStrategy} = await setup();
-        console.log(vanillaVotingStrategy.address);
         const {voting_power: vp} = await vanillaVotingStrategy.call("get_voting_power", {block: 1, account_160: 2,  params: []});
         expect(new SplitUint256(vp.low, vp.high)).to.deep.equal(SplitUint256.fromUint(BigInt(1)));
     }).timeout(60000);
