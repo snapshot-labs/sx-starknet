dd15310b4c050e9d8ab0b0cbf4782caba0d8390c
Scott Piriou <30843220+pscott@users.noreply.github.com>
2022-03-03 14:55:52 +0100

    Add test for proposal

--- test/starknet/vanilla_authenticator.ts
+++ test/starknet/vanilla_authenticator.ts
@@ -3,10 +3,10 @@ import { starknet } from 'hardhat';
 import { stark } from 'starknet';
 const { getSelectorFromName } = stark;
 
-const EXECUTE_METHOD = "execute"
-const PROPOSAL_METHOD = "propose"
-const VOTE_METHOD = "vote"
-const SPACE_CONTRACT = BigInt(1337)
+const EXECUTE_METHOD = 'execute';
+const PROPOSAL_METHOD = 'propose';
+const VOTE_METHOD = 'vote';
+const SPACE_CONTRACT = BigInt(1337);
 
 async function setup() {
   const vanillaAuthenticatorFactory = await starknet.getContractFactory(
@@ -19,12 +19,22 @@ async function setup() {
 }
 
 describe('Authenticator execute call:', () => {
-  it('Gets called correctly', async () => {
+  it('Calls the vote methd correctly', async () => {
     const { vanillaAuthenticator } = await setup();
 
     await vanillaAuthenticator.call(EXECUTE_METHOD, {
       to: SPACE_CONTRACT,
       function_selector: BigInt(getSelectorFromName(VOTE_METHOD)),
+      calldata: [],
+    });
+  }).timeout(60000);
+
+  it('Calls the proposal method correctly', async () => {
+    const { vanillaAuthenticator } = await setup();
+
+    await vanillaAuthenticator.call(EXECUTE_METHOD, {
+      to: SPACE_CONTRACT,
+      function_selector: BigInt(getSelectorFromName(PROPOSAL_METHOD)),
       calldata: [],
     });
   }).timeout(60000);
