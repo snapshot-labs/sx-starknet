129c081fd334b439080a0a013957b1f2b9a463b2
Scott Piriou <30843220+pscott@users.noreply.github.com>
2022-03-08 15:27:29 +0100

    Add some logging during tests

--- test/starknet/vanilla_space.ts
+++ test/starknet/vanilla_space.ts
@@ -26,10 +26,13 @@ async function setup() {
   const vanillaAuthenticatorFactory = await starknet.getContractFactory(
     './contracts/starknet/authenticator/authenticator.cairo'
   );
+  console.log("Deploying auth...");
   const vanillaAuthenticator = (await vanillaAuthenticatorFactory.deploy()) as StarknetContract;
+  console.log("Deploying strat...");
   const vanillaVotingStrategy = (await vanillaVotingStategyFactory.deploy()) as StarknetContract;
   const voting_strategy = BigInt(vanillaVotingStrategy.address);
   const authenticator = BigInt(vanillaAuthenticator.address);
+  console.log("Deploying space...");
   const vanillaSpace = (await vanillaSpaceFactory.deploy({
     _voting_delay: VOTING_DELAY,
     _voting_period: VOTING_PERIOD,
@@ -47,6 +50,7 @@ async function setup() {
 
 describe('Space testing', () => {
   it('Simple Vote', async () => {
+    console.log("Setup...");
     const { vanillaSpace, vanillaAuthenticator, vanillaVotingStrategy } = await setup();
     const space_contract = BigInt(vanillaSpace.address);
     const execution_hash = BigInt(1);
@@ -58,6 +62,7 @@ describe('Space testing', () => {
 
     // -- Creates the proposal --
     {
+      console.log("Creating proposal...");
       await vanillaAuthenticator.invoke(EXECUTE_METHOD, {
         to: space_contract,
         function_selector: BigInt(getSelectorFromName(PROPOSAL_METHOD)),
@@ -70,6 +75,8 @@ describe('Space testing', () => {
         ],
       });
 
+
+      console.log("Getting proposal info...");
       const { proposal_info } = await vanillaSpace.call('get_proposal_info', {
         proposal_id: proposal_id,
       });
@@ -94,12 +101,14 @@ describe('Space testing', () => {
     {
       const voter_address = proposer_address;
       const params: Array<BigInt> = [];
+      console.log("Voting FOR...");
       await vanillaAuthenticator.invoke(EXECUTE_METHOD, {
         to: space_contract,
         function_selector: BigInt(getSelectorFromName(VOTE_METHOD)),
         calldata: [voter_address, proposal_id, FOR, BigInt(params.length)],
       });
 
+      console.log("Getting proposal info...");
       const { proposal_info } = await vanillaSpace.call('get_proposal_info', {
         proposal_id: proposal_id,
       });
@@ -111,7 +120,7 @@ describe('Space testing', () => {
       const abstain = SplitUint256.fromObj(proposal_info.power_abstain).toUint();
       expect(abstain).to.deep.equal(BigInt(0));
     }
-  }).timeout(60000);
+  }).timeout(6000000);
 
   // TODO: how can we do tests that are expected to fail?
   // it('Fails if proposing without going through the authenticator', async () => {
