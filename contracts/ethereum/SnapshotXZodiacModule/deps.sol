b46da9fd679b1e362c541a8f4314603f4b75897a
Orland0x <37511817+Orland0x@users.noreply.github.com>
2022-04-02 13:41:40 +0100

    fixes

--- contracts/ethereum/SnapshotXZodiacModule/deps.sol
+++ contracts/ethereum/SnapshotXZodiacModule/deps.sol
@@ -5,3 +5,5 @@ pragma solidity 0.8.9;
 import '@gnosis.pm/safe-contracts/contracts/GnosisSafeL2.sol';
 import '@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol';
 import '@gnosis.pm/zodiac/contracts/factory/ModuleProxyFactory.sol';
+import '@gnosis.pm/zodiac/contracts/guard/Guardable.sol';
+import '@gnosis.pm/zodiac/contracts/core/Module.sol';
