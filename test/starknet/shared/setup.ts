a3d3b6f9adeba06944808b62724815f941dff477
Scott Piriou <30843220+pscott@users.noreply.github.com>
2022-05-03 15:33:28 +0200

    Format

--- test/starknet/shared/setup.ts
+++ test/starknet/shared/setup.ts
@@ -19,9 +19,7 @@ export const VITALIK_STRING_ADDRESS = VITALIK_ADDRESS.toString(16);
 export async function vanillaSetup() {
   const account = await starknet.deployAccount('OpenZeppelin');
 
-  const vanillaSpaceFactory = await starknet.getContractFactory(
-    './contracts/starknet/space.cairo'
-  );
+  const vanillaSpaceFactory = await starknet.getContractFactory('./contracts/starknet/space.cairo');
   const vanillaVotingStategyFactory = await starknet.getContractFactory(
     './contracts/starknet/voting_strategies/vanilla.cairo'
   );
