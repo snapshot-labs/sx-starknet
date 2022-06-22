b9008a561c79e2110d0319de54cc79a1e8770e10
Orland0x <37511817+Orland0x@users.noreply.github.com>
2022-06-22 17:03:29 +0100

    test for each controller action

--- test/starknet/ControllerActions.test.ts
+++ test/starknet/ControllerActions.test.ts
@@ -3,6 +3,7 @@ import { starknet } from 'hardhat';
 import { vanillaSetup } from '../shared/setup';
 import { StarknetContract, Account } from 'hardhat/types';
 import { flatten2DArray } from '../shared/helpers';
+import { SplitUint256 } from '../shared/types';
 
 describe('Controller Actions', () => {
   let space: StarknetContract;
@@ -46,6 +47,56 @@ describe('Controller Actions', () => {
     });
     await controller.invoke(space, 'remove_voting_strategies', {
       to_remove: votingStrategies,
+    });
+  }).timeout(600000);
+
+  it('The controller can add and remove authenticators', async () => {
+    const authenticators: bigint[] = [BigInt(1234), BigInt(4567)];
+    await controller.invoke(space, 'add_authenticators', {
+      to_add: authenticators,
+    });
+    await controller.invoke(space, 'remove_authenticators', {
+      to_remove: authenticators,
+    });
+  }).timeout(600000);
+
+  it('The controller can add and remove execution strategies', async () => {
+    const executionStrategies: bigint[] = [BigInt(1234), BigInt(4567)];
+    await controller.invoke(space, 'add_executors', {
+      to_add: executionStrategies,
+    });
+    await controller.invoke(space, 'remove_executors', {
+      to_remove: executionStrategies,
+    });
+  }).timeout(600000);
+
+  it('The controller can update the quorum', async () => {
+    await controller.invoke(space, 'update_quorum', {
+      new_quorum: SplitUint256.fromUint(BigInt(1234)),
+    });
+  }).timeout(600000);
+
+  it('The controller can update the voting delay', async () => {
+    await controller.invoke(space, 'update_voting_delay', {
+      new_delay: BigInt(1234),
+    });
+  }).timeout(600000);
+
+  it('The controller can update the min voting duration', async () => {
+    await controller.invoke(space, 'update_min_voting_duration', {
+      new_min_duration: BigInt(1234),
+    });
+  }).timeout(600000);
+
+  it('The controller can update the max voting duration', async () => {
+    await controller.invoke(space, 'update_max_voting_duration', {
+      new_max_duration: BigInt(1234),
+    });
+  }).timeout(600000);
+
+  it('The controller can update the proposal threshold', async () => {
+    await controller.invoke(space, 'update_proposal_threshold', {
+      new_threshold: SplitUint256.fromUint(BigInt(1234)),
     });
   }).timeout(600000);
 });
