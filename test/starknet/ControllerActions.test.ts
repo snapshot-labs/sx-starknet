5ba063f922d2f518e161f795b1d37c5a08e55f1a
Scott Piriou <30843220+pscott@users.noreply.github.com>
2022-08-11 12:20:37 +0200

    Add authenticator test

--- test/starknet/ControllerActions.test.ts
+++ test/starknet/ControllerActions.test.ts
@@ -37,25 +37,114 @@ describe('Controller Actions', () => {
   }).timeout(600000);
 
   it('The controller can add and remove voting strategies', async () => {
-    const votingStrategies: string[] = ['0x1234', '0x4567'];
-    const votingStrategyParams: string[][] = [['0x1', '0x2', '0x0'], []];
+    const address = ethers.Wallet.createRandom().address;
+
+    const power = utils.splitUint256.SplitUint256.fromUint(BigInt('1000'));
+
+    const whitelistFactory = await starknet.getContractFactory(
+      './contracts/starknet/VotingStrategies/Whitelist.cairo'
+    );
+    const whitelistStrategy = await whitelistFactory.deploy({
+      _whitelist: [
+        address,
+        power.low,
+        power.high,
+      ],
+    });
+    const votingStrategyParams: string[][] = [[]];
     const votingStrategyParamsFlat: string[] = utils.encoding.flatten2DArray(votingStrategyParams);
+    // Add the whitelist strategy
     await controller.invoke(space, 'add_voting_strategies', {
-      to_add: votingStrategies,
+      to_add: [whitelistStrategy.address],
       params_flat: votingStrategyParamsFlat,
     });
+
+    // Remove the vanilla voting strategy
     await controller.invoke(space, 'remove_voting_strategies', {
-      to_remove: votingStrategies,
+      to_remove: [vanillaVotingStrategy.address],
+    });
+
+    // Ensure that `controller` can't propose (we removed the vanillaVotingStrategy)
+    try {
+      const wrongProposeCalldata = getProposeCalldata(controller.address, utils.intsSequence.IntsSequence.LEFromString(""), vanillaExecutionStrategy.address, [vanillaVotingStrategy.address], [[]], []);
+      await controller.invoke(vanillaAuthenticator, "authenticate", {
+        target: space.address,
+        function_selector: PROPOSE_SELECTOR,
+        calldata: wrongProposeCalldata,
+      });
+    } catch (error: any) {
+      expect(error.message).to.contain('Invalid voting strategy');
+    }
+
+    // Ensure that `address` can propose (we added the whitelist strategy)
+    const correctProposeCalldata = getProposeCalldata(address, utils.intsSequence.IntsSequence.LEFromString(""), vanillaExecutionStrategy.address, [whitelistStrategy.address], [[]], []);
+    await controller.invoke(vanillaAuthenticator, "authenticate", {
+      target: space.address,
+      function_selector: PROPOSE_SELECTOR,
+      calldata: correctProposeCalldata,
+    });
+
+    // Ensure proposal exists (will throw if proposal id does not exist)
+    await space.call("get_proposal_info", {
+      proposal_id: 1,
+    });
+
+    // Cancel the proposal
+    await controller.invoke(space, "cancel_proposal", {
+      proposal_id: 1,
+      execution_params: [],
+    });
+
+    // Put back the original strategy
+    await controller.invoke(space, 'add_voting_strategies', {
+      to_add: [vanillaVotingStrategy.address],
+      params_flat: votingStrategyParamsFlat,
+    });
+    
+    // Remove the vanilla voting strategy
+    await controller.invoke(space, 'remove_voting_strategies', {
+      to_remove: [whitelistStrategy.address],
     });
   }).timeout(600000);
 
   it('The controller can add and remove authenticators', async () => {
-    const authenticators: bigint[] = [BigInt(1234), BigInt(4567)];
+    const starknetTxAuthenticatorFactory = await starknet.getContractFactory(
+      './contracts/starknet/Authenticators/StarkTx.cairo'
+    );
+    const starknetTxAuth = await starknetTxAuthenticatorFactory.deploy() as StarknetContract;
+
+    // Add the StarknetTx auth
     await controller.invoke(space, 'add_authenticators', {
-      to_add: authenticators,
+      to_add: [starknetTxAuth.address],
     });
+
+    // Remove the Vanilla Auth
     await controller.invoke(space, 'remove_authenticators', {
-      to_remove: authenticators,
+      to_remove: [vanillaAuthenticator.address],
+    });
+
+    const proposeCalldata = getProposeCalldata(user.address, utils.intsSequence.IntsSequence.LEFromString(""), vanillaExecutionStrategy.address, [vanillaVotingStrategy.address], [[]], []);
+    // Ensure that `controller` can't propose (we removed the vanillaAuth)
+    try {
+      await controller.invoke(vanillaAuthenticator, "authenticate", {
+        target: space.address,
+        function_selector: PROPOSE_SELECTOR,
+        calldata: proposeCalldata,
+      });
+    } catch (error: any) {
+      expect(error.message).to.contain('Invalid authenticator');
+    }
+
+    // Ensure that `address` can propose (we added the StarknetTxAuth)
+    await user.invoke(starknetTxAuth, "authenticate", {
+      target: space.address,
+      function_selector: PROPOSE_SELECTOR,
+      calldata: proposeCalldata,
+    });
+
+    // Ensure proposal exists (will throw if proposal id does not exist)
+    const { proposal_info } =  await space.call("get_proposal_info", {
+      proposal_id: 1,
     });
   }).timeout(600000);
 
