de74ff6aa27bc73562c51652578c963b5932af54
Scott Piriou <30843220+pscott@users.noreply.github.com>
2022-08-28 11:37:30 +0200

    Add starknet signature and corresponding tests

---
+++ test/shared/starkTypes.ts
@@ -0,0 +1,65 @@
+export const domain = {
+  name: 'snapshot-x',
+  version: '1',
+};
+
+export const domainTypes = {
+  StarkNetDomain: [
+    {
+      name: 'name',
+      type: 'felt',
+    },
+    {
+      name: 'version',
+      type: 'felt',
+    },
+  ],
+};
+
+export const proposeTypes = {
+  StarkNetDomain: domainTypes.StarkNetDomain,
+  Propose: [
+    { name: 'space', type: 'felt' },
+    { name: 'proposerAddress', type: 'felt' },
+    { name: 'metadataURI', type: 'felt' },
+    { name: 'executor', type: 'felt' },
+    { name: 'executionParamsHash', type: 'felt' },
+    { name: 'usedVotingStrategiesHash', type: 'felt' },
+    { name: 'userVotingStrategyParamsFlatHash', type: 'felt' },
+    { name: 'salt', type: 'felt' },
+  ],
+};
+
+export const voteTypes = {
+  StarkNetDomain: domainTypes.StarkNetDomain,
+  Vote: [
+    { name: 'space', type: 'felt' },
+    { name: 'voterAddress', type: 'felt' },
+    { name: 'proposal', type: 'felt' },
+    { name: 'choice', type: 'felt' },
+    { name: 'usedVotingStrategiesHash', type: 'felt' },
+    { name: 'userVotingStrategyParamsFlatHash', type: 'felt' },
+    { name: 'salt', type: 'felt' },
+  ],
+};
+
+export interface Propose {
+  space: string;
+  proposerAddress: string;
+  metadataURI: string;
+  executor: string;
+  executionParamsHash: string;
+  usedVotingStrategiesHash: string;
+  userVotingStrategyParamsFlatHash: string;
+  salt: string;
+}
+
+export interface Vote {
+  space: string;
+  voterAddress: string;
+  proposal: string;
+  choice: number;
+  usedVotingStrategiesHash: string;
+  userVotingStrategyParamsFlatHash: string;
+  salt: string;
+}
