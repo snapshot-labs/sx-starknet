73f5b9ccc545191cd77cf0cf84f2885d3733209a
Orlando <orlando@snapshot.org>
2023-02-03 16:25:02 +0000

    feat: updated setups

--- test/shared/setup.ts
+++ test/shared/setup.ts
@@ -244,6 +244,95 @@ export async function safeWithZodiacSetup(
   };
 }
 
+export async function safeWithZodiacSetup2(
+  starknetCoreAddress = '0x0000000000000000000000000000000000000001',
+  spaceAddress = BigInt(1),
+  zodiacRelayerAddress = BigInt(1)
+) {
+  const wallets = await ethers.getSigners();
+  const safeSigner = wallets[0]; // One 1 signer on the safe
+
+  const GnosisSafeL2 = await ethers.getContractFactory(
+    '@gnosis.pm/safe-contracts/contracts/GnosisSafeL2.sol:GnosisSafeL2'
+  );
+  const FactoryContract = await ethers.getContractFactory(
+    '@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol:GnosisSafeProxyFactory'
+  );
+  const singleton = await GnosisSafeL2.deploy();
+  const factory = await FactoryContract.deploy();
+
+  const template = await factory.callStatic.createProxy(singleton.address, '0x');
+  await factory.createProxy(singleton.address, '0x');
+
+  const safe = GnosisSafeL2.attach(template);
+  safe.setup([safeSigner.address], 1, AddressZero, '0x', AddressZero, AddressZero, 0, AddressZero);
+
+  const moduleFactoryContract = await ethers.getContractFactory('ModuleProxyFactory');
+  const moduleFactory = await moduleFactoryContract.deploy();
+
+  const SnapshotXContract = await ethers.getContractFactory('SXAvatarExecutor');
+
+  //deploying singleton master contract
+  const masterzodiacModule = await SnapshotXContract.deploy(
+    '0x0000000000000000000000000000000000000001',
+    '0x0000000000000000000000000000000000000001',
+    '0x0000000000000000000000000000000000000001',
+    1,
+    []
+  );
+
+  const encodedInitParams = ethers.utils.defaultAbiCoder.encode(
+    ['address', 'address', 'address', 'uint256', 'uint256[]'],
+    [
+      safe.address,
+      safe.address,
+      starknetCoreAddress,
+      zodiacRelayerAddress,
+      [spaceAddress],
+    ]
+  );
+
+  const initData = masterzodiacModule.interface.encodeFunctionData('setUp', [encodedInitParams]);
+
+  const masterCopyAddress = masterzodiacModule.address.toLowerCase().replace(/^0x/, '');
+
+  //This is the bytecode of the module proxy contract
+  const byteCode =
+    '0x602d8060093d393df3363d3d373d3d3d363d73' +
+    masterCopyAddress +
+    '5af43d82803e903d91602b57fd5bf3';
+
+  const salt = ethers.utils.solidityKeccak256(
+    ['bytes32', 'uint256'],
+    [ethers.utils.solidityKeccak256(['bytes'], [initData]), '0x01']
+  );
+
+  const expectedAddress = ethers.utils.getCreate2Address(
+    moduleFactory.address,
+    salt,
+    ethers.utils.keccak256(byteCode)
+  );
+
+  expect(await moduleFactory.deployModule(masterzodiacModule.address, initData, '0x01'))
+    .to.emit(moduleFactory, 'ModuleProxyCreation')
+    .withArgs(expectedAddress, masterzodiacModule.address);
+  const zodiacModule = SnapshotXContract.attach(expectedAddress);
+
+  await executeContractCallWithSigners(
+    safe,
+    safe,
+    'enableModule',
+    [zodiacModule.address],
+    [safeSigner]
+  );
+
+  return {
+    zodiacModule: zodiacModule as Contract,
+    safe: safe as Contract,
+    safeSigner: safeSigner as SignerWithAddress,
+  };
+}
+
 export async function ethTxAuthSetup() {
   const controller = (await starknet.deployAccount('OpenZeppelin')) as Account;
   const spaceFactory = await starknet.getContractFactory('./contracts/starknet/SpaceAccount.cairo');
