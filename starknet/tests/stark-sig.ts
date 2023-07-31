12d6ba3fa28d65a1cd242d602720d3a09556eb12
Orlando <orlando@snapshot.org>
2023-07-31 12:43:58 +0100

    chore: cleaned up stark sig test

--- starknet/tests/stark-sig.ts
+++ starknet/tests/stark-sig.ts
@@ -1,85 +1,62 @@
 import fs from "fs";
 import dotenv from 'dotenv';
 import { Provider, Account, Contract, CallData, Calldata, typedData, cairo}  from "starknet";
-import { typedDataPropose } from "./types";
+import { proposeTypes, Propose, StarknetSigProposeCalldata, domain } from "./types";
 
 dotenv.config();
 
 const pk = process.env.PRIVATE_KEY || '';
 
 async function main() {
-    // connect provider
     const provider = new Provider({ sequencer: { baseUrl:"http://127.0.0.1:5050"} });
-    // new Open Zeppelin account v0.5.1 :
-        // Generate public and private key pair.
+
     const privateKey0 = "0xe3e70682c2094cac629f6fbed82c07cd";
     const address0 = "0x7e00d496e324876bbc8531f2d9a82bf154d1a04a50218ee74cdd372f75a551a";
+    const publickey0 = "0x7e52885445756b313ea16849145363ccb73fb4ab0440dbac333cf9d13de82b9"
     const account0 = new Account(provider, address0, privateKey0);
-
-    const starkSigAuthAddress = "0x14f48340c1ee431755c004365b58e92e69003495af214cf90c3603338e7e945";
+    
+    const starkSigAuthAddress = "0x2baf1877b1388d8421485c8cb419b37ebce3096e323c4ef6b3c979a8a30917e";
 
     const {abi: starkSigAuthAbi} = await provider.getClassAt(starkSigAuthAddress);
     const starkSigAuth = new Contract(starkSigAuthAbi, starkSigAuthAddress, provider);
 
-    const msgHash = typedData.getMessageHash(typedDataPropose, address0);
+    const proposeMsg: Propose = {
+        space: "0x0000000000000000000000000000000000007777",
+        author: "0x7e00d496e324876bbc8531f2d9a82bf154d1a04a50218ee74cdd372f75a551a",
+        executionStrategy: {
+            address: "0x0000000000000000000000000000000000001234",
+            params: ["0x5", "0x6", "0x7", "0x8"]
+        },
+        userProposalValidationParams: ["0x1", "0x2", "0x3", "0x4"],
+        salt: "0x0"
+    }
+
+    const proposeData: typedData.TypedData = {
+        types: proposeTypes,
+        primaryType: "Propose",
+        domain: domain,
+        message: proposeMsg as any
+    }
+
+    const msgHash = typedData.getMessageHash(proposeData, address0);
 
     console.log('msgHash=', msgHash);
 
-    const signature2 = await account0.signMessage(typedDataPropose);
+    const signature2 = await account0.signMessage(proposeData) as any;
 
-    console.log('signature2=', signature2);
-
-    // console.log(starkSigAuth.abi[2]);
-    const specialParameters: Calldata = CallData.compile({
-                r: 1,
-                s: 2,
-                target: "0x0000000000000000000000000000000000007777",
-                author: address0,
-                execution_strategy: {
-                    addr: "0x0000000000000000000000000000000000001234",
-                    params: [5,6,7,8]
-                },
-                user_proposal_validation_params: [1,2,3,4],
-                salt: 0
-            });
+    const proposeCalldata: StarknetSigProposeCalldata = {
+        r: signature2.r,    
+        s: signature2.s,
+        ...proposeMsg,
+        public_key: publickey0
+    }
     
-    // console.log(specialParameters);
-        
-    const out = await starkSigAuth.call("propose_hash", specialParameters, {parseResponse: false});
-
-    console.log('out=', out);
-
-
-    // // const result = await account0.execute({
-    // //     contractAddress: starkSigAuthAddress,
-    // //     entrypoint: "propose_hash",
-    // //     calldata: CallData.compile({
-    // //         r: 1,
-    // //         s: 2,
-    // //         target: "0x0000000000000000000000000000000000001235",
-    // //         author: address0,
-    // //         execution_strategy: {
-    // //             addr: "0x0000000000000000000000000000000000001234",
-    // //             params: [1,2,3,4]
-    // //         },
-    // //         user_proposal_validation_params: [1,2,3,4],
-    // //         salt: 0
-    // //     })
-    // // })
-    // // console.log(result);
-
-
-    // // const proposeMessage: Propose = {
-    // //     space: "0x0000000000000000000000000000000000001234",
-    // //     author: signer.address,
-    // //     executionStrategy: {
-    // //         addr: "0x0000000000000000000000000000000000001234",
-    // //         params: "0x1234"
-    // //     },
-    // //     userProposalValidationParams: "0x1234",
-    // //     salt: "0x0"
-    // // }
-
+    const result = await account0.execute({
+        contractAddress: starkSigAuthAddress,
+        entrypoint: "authenticate_propose",
+        calldata: CallData.compile(proposeCalldata as any)
+    })
+    console.log(result);
 }
 
 main()
--- starknet/tests/types.ts
+++ starknet/tests/types.ts
@@ -1,37 +1,85 @@
-import { TypedData }  from "starknet";
-export const typedDataPropose: TypedData = {
-    types: {
-        StarkNetDomain: [
-            { name: "name", type: "felt252" },
-            { name: "version", type: "felt252" },
-            { name: "chainId", type: "felt252" },
-        ],
-        Strategy: [
-            { name: "address", type: "felt252" },
-            { name: "params", type: "felt*" }
-        ],
-        Propose: [
-            { name: 'space', type: 'ContractAddress' },
-            { name: 'author', type: 'ContractAddress' },
-            { name: 'executionStrategy', type: 'Strategy' },
-            { name: 'userProposalValidationParams', type: 'felt*' },
-            { name: 'salt', type: 'felt252' }
-        ]
-    },
-    primaryType: "Propose",
-    domain: {
-        name: "1", // put the name of your dapp to ensure that the signatures will not be used by other DAPP
-        version: "1",
-        chainId: "0x534e5f474f45524c49" // devnet id 
-    },
-    message: {
-        space: "0x0000000000000000000000000000000000007777",
-        author: "0x7e00d496e324876bbc8531f2d9a82bf154d1a04a50218ee74cdd372f75a551a",
-        executionStrategy: {
-            address: "0x0000000000000000000000000000000000001234",
-            params: [5,6,7,8]
-        },
-        userProposalValidationParams: ["0x1", "0x2", "0x3", "0x4"],
-        salt: "0x0"
-    },
-};
+// import { TypedData }  from "starknet";
+// export const typedDataPropose: TypedData = {
+//     types: {
+//         StarkNetDomain: [
+//             { name: "name", type: "felt252" },
+//             { name: "version", type: "felt252" },
+//             { name: "chainId", type: "felt252" },
+//         ],
+//         Strategy: [
+//             { name: "address", type: "felt252" },
+//             { name: "params", type: "felt*" }
+//         ],
+//         Propose: [
+//             { name: 'space', type: 'ContractAddress' },
+//             { name: 'author', type: 'ContractAddress' },
+//             { name: 'executionStrategy', type: 'Strategy' },
+//             { name: 'userProposalValidationParams', type: 'felt*' },
+//             { name: 'salt', type: 'felt252' }
+//         ]
+//     },
+//     primaryType: "Propose",
+//     domain: {
+//         name: "1", // put the name of your dapp to ensure that the signatures will not be used by other DAPP
+//         version: "1",
+//         chainId: "0x534e5f474f45524c49" // devnet id 
+//     },
+//     message: {
+//         space: "0x0000000000000000000000000000000000007777",
+//         author: "0x7e00d496e324876bbc8531f2d9a82bf154d1a04a50218ee74cdd372f75a551a",
+//         executionStrategy: {
+//             address: "0x0000000000000000000000000000000000001234",
+//             params: [5,6,7,8]
+//         },
+//         userProposalValidationParams: ["0x1", "0x2", "0x3", "0x4"],
+//         salt: "0x0"
+//     },
+// };
+
+export const domain = {
+    name: "1", // put the name of your dapp to ensure that the signatures will not be used by other DAPP
+    version: "1",
+    chainId: "0x534e5f474f45524c49" // devnet id 
+}
+
+export const domainTypes = {
+    StarkNetDomain: [
+        { name: "name", type: "felt252" },
+        { name: "version", type: "felt252" },
+        { name: "chainId", type: "felt252" },
+    ],
+  };
+
+export const proposeTypes = {
+    StarkNetDomain: domainTypes.StarkNetDomain,
+    Propose: [
+        { name: 'space', type: 'ContractAddress' },
+        { name: 'author', type: 'ContractAddress' },
+        { name: 'executionStrategy', type: 'Strategy' },
+        { name: 'userProposalValidationParams', type: 'felt*' },
+        { name: 'salt', type: 'felt252' }
+    ],
+    Strategy: [
+        { name: "address", type: "felt252" },
+        { name: "params", type: "felt*" }
+    ],
+  };
+
+export interface Strategy {
+    address: string;
+    params: string[];
+}
+
+export interface Propose {
+    space: string;
+    author: string;
+    executionStrategy: Strategy;
+    userProposalValidationParams: string[];
+    salt: string;
+}
+
+export interface StarknetSigProposeCalldata extends Propose {
+    r: string;
+    s: string;
+    public_key: string;
+}
