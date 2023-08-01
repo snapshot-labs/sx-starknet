d47b948da388579bea45789c3a9e516d22b3b9a8
Orlando <orlando@snapshot.org>
2023-08-01 16:17:57 +0100

    fix(StarknetCommit): commit event + rename

--- ethereum/script/deployer.s.sol
+++ ethereum/script/deployer.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.18;
 
 import "forge-std/Script.sol";
-import "../src/StarkNetCommit.sol";
+import "../src/StarknetCommit.sol";
 
 interface SingletonFactory {
     function deploy(bytes memory _initCode, bytes32 salt) external returns (address payable);
@@ -13,11 +13,11 @@ contract Deployer is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
         SingletonFactory singletonFactory = SingletonFactory(0xce0042B868300000d44A59004Da54A005ffdcf9f);
-        bytes32 salt = bytes32(uint256(2));
+        bytes32 salt = bytes32(uint256(0));
 
         singletonFactory.deploy(
             abi.encodePacked(
-                type(StarkNetCommit).creationCode, abi.encode(address(0xde29d060D45901Fb19ED6C6e959EB22d8626708e))
+                type(StarknetCommit).creationCode, abi.encode(address(0xde29d060D45901Fb19ED6C6e959EB22d8626708e))
             ),
             salt
         );
--- ethereum/src/StarknetCommit.sol
+++ ethereum/src/StarknetCommit.sol
@@ -6,13 +6,16 @@ import "./interfaces/IStarknetCore.sol";
 
 /// @title Starknet Commit Contract
 /// @notice Allows data to be committed to Starknet via a transaction on L1. The contract works in combination with a corresponding authenticator contract on Starknet.
-contract StarkNetCommit {
+contract StarknetCommit {
     /// @notice The Starknet core contract.
     IStarknetCore public immutable starknetCore;
 
     /// @dev Selector for the L1 handler in the authenticator on Starknet:
     uint256 private constant L1_COMMIT_HANDLER =
         674623595553689999852507866835294387286428733459551884504121875060358224925;
+
+    /// @notice Emitted when a hash is committed.
+    event Commit(uint256 starknetAuthenticator, uint256 _hash, bytes32 msgHash, uint256 msgNonce);
 
     constructor(IStarknetCore _starknetCore) {
         starknetCore = _starknetCore;
@@ -25,6 +28,7 @@ contract StarkNetCommit {
         uint256[] memory payload = new uint256[](2);
         payload[0] = uint256(uint160(msg.sender));
         payload[1] = _hash;
-        starknetCore.sendMessageToL2(starknetAuthenticator, L1_COMMIT_HANDLER, payload);
+        (bytes32 msgHash, uint256 msgNonce) = starknetCore.sendMessageToL2(starknetAuthenticator, L1_COMMIT_HANDLER, payload);
+        emit Commit(starknetAuthenticator, _hash, msgHash, msgNonce);
     }
 }
