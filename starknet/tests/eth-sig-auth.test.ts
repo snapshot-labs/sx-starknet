91ffd41fa862d50c958a4a1fff7a94a169747c6f
Orlando <orlando@snapshot.org>
2023-08-11 14:33:04 +0100

    chore: cleaanup and formatting

--- starknet/tests/eth-sig-auth.test.ts
+++ starknet/tests/eth-sig-auth.test.ts
@@ -1,7 +1,14 @@
 import fs from 'fs';
-import { ethers } from "ethers";
-import { Provider, Account, Contract, CallData, cairo, shortString, json, Calldata } from 'starknet';
-import { Propose, proposeTypes, EthereumSigProposeCalldata, voteTypes, Vote, updateProposalTypes, UpdateProposal} from "./types";
+import { ethers } from 'ethers';
+import { Provider, Account, CallData, cairo, json } from 'starknet';
+import {
+  Propose,
+  proposeTypes,
+  voteTypes,
+  Vote,
+  updateProposalTypes,
+  UpdateProposal,
+} from './types';
 
 const pk = process.env.PRIVATE_KEY || '';
 
@@ -13,22 +20,93 @@ describe('Starknet Signature Authenticator', () => {
   const address0 = '0x7e00d496e324876bbc8531f2d9a82bf154d1a04a50218ee74cdd372f75a551a';
   const account0 = new Account(provider, address0, privateKey0);
 
+  let spaceAddress: string;
+  let vanillaVotingStrategyAddress: string;
+  let vanillaProposalValidationStrategyAddress: string;
   let ethSigAuthAddress: string;
-  let ethSigAuth: Contract;
   let domain: any;
 
   beforeAll(async () => {
     // Deploy Ethereum Signature Authenticator
-    const ethSigAuthSierra = json.parse(fs.readFileSync( "starknet/target/dev/sx_EthSigAuthenticator.sierra.json").toString( "ascii"));
-    const ethSigAuthCasm = json.parse(fs.readFileSync( "starknet/target/dev/sx_EthSigAuthenticator.casm.json").toString( "ascii"));
+    const ethSigAuthSierra = json.parse(
+      fs.readFileSync('starknet/target/dev/sx_EthSigAuthenticator.sierra.json').toString('ascii'),
+    );
+    const ethSigAuthCasm = json.parse(
+      fs.readFileSync('starknet/target/dev/sx_EthSigAuthenticator.casm.json').toString('ascii'),
+    );
 
-    const deployResponse = await account0.declareAndDeploy({ contract: ethSigAuthSierra, casm: ethSigAuthCasm, constructorCalldata: CallData.compile({})});
+    let deployResponse = await account0.declareAndDeploy({
+      contract: ethSigAuthSierra,
+      casm: ethSigAuthCasm,
+      constructorCalldata: CallData.compile({}),
+    });
 
     ethSigAuthAddress = deployResponse.deploy.contract_address;
 
-    const {abi: ethSigAuthAbi} = await provider.getClassAt(ethSigAuthAddress);
-    ethSigAuth = new Contract(ethSigAuthAbi, ethSigAuthAddress, provider);
- 
+    // Deploy Vanilla Voting Strategy
+    const vanillaVotingStrategySierra = json.parse(
+      fs.readFileSync('starknet/target/dev/sx_VanillaVotingStrategy.sierra.json').toString('ascii'),
+    );
+    const vanillaVotingStrategyCasm = json.parse(
+      fs.readFileSync('starknet/target/dev/sx_VanillaVotingStrategy.casm.json').toString('ascii'),
+    );
+
+    deployResponse = await account0.declareAndDeploy({
+      contract: vanillaVotingStrategySierra,
+      casm: vanillaVotingStrategyCasm,
+      constructorCalldata: CallData.compile({}),
+    });
+    vanillaVotingStrategyAddress = deployResponse.deploy.contract_address;
+
+    // Deploy Vanilla Proposal Validation Strategy
+    const vanillaProposalValidationStrategySierra = json.parse(
+      fs
+        .readFileSync('starknet/target/dev/sx_VanillaProposalValidationStrategy.sierra.json')
+        .toString('ascii'),
+    );
+    const vanillaProposalValidationStrategyCasm = json.parse(
+      fs
+        .readFileSync('starknet/target/dev/sx_VanillaProposalValidationStrategy.casm.json')
+        .toString('ascii'),
+    );
+
+    deployResponse = await account0.declareAndDeploy({
+      contract: vanillaProposalValidationStrategySierra,
+      casm: vanillaProposalValidationStrategyCasm,
+      constructorCalldata: CallData.compile({}),
+    });
+    vanillaProposalValidationStrategyAddress = deployResponse.deploy.contract_address;
+
+    // Deploy Space
+    const spaceSierra = json.parse(
+      fs.readFileSync('starknet/target/dev/sx_Space.sierra.json').toString('ascii'),
+    );
+    const spaceCasm = json.parse(
+      fs.readFileSync('starknet/target/dev/sx_Space.casm.json').toString('ascii'),
+    );
+
+    deployResponse = await account0.declareAndDeploy({
+      contract: spaceSierra,
+      casm: spaceCasm,
+      constructorCalldata: CallData.compile({
+        _owner: 1,
+        _max_voting_duration: 100,
+        _min_voting_duration: 100,
+        _voting_delay: 1,
+        _proposal_validation_strategy: {
+          address: vanillaProposalValidationStrategyAddress,
+          params: [[]],
+        },
+        _proposal_validation_strategy_metadata_URI: [],
+        _voting_strategies: [{ address: vanillaVotingStrategyAddress, params: [] }],
+        _voting_strategies_metadata_URI: [],
+        _authenticators: [ethSigAuthAddress],
+        _metadata_URI: [],
+        _dao_URI: [],
+      }),
+    });
+    spaceAddress = deployResponse.deploy.contract_address;
+
     domain = {
       chainId: '0x534e5f474f45524c49', // devnet id
     };
@@ -37,123 +115,143 @@ describe('Starknet Signature Authenticator', () => {
     // PROPOSE
 
     const proposeMsg: Propose = {
-        authenticator: ethSigAuthAddress,        
-        space: "0x0000000000000000000000000000000000001234",
-        author: signer.address,
-        executionStrategy: {
-            address: "0x0000000000000000000000000000000000005678",
-            params: ["0x0"]
-        },
-        userProposalValidationParams: ["0xffffffffffffffffffffffffffffffffffffffffff", "0x1234", "0x5678", "0x9abc"],
-        metadataURI: ["0x1", "0x2", "0x3", "0x4"],
-        salt: "0x7"
-    }
+      authenticator: ethSigAuthAddress,
+      space: spaceAddress,
+      author: signer.address,
+      executionStrategy: {
+        address: '0x0000000000000000000000000000000000005678',
+        params: ['0x0'],
+      },
+      userProposalValidationParams: [
+        '0xffffffffffffffffffffffffffffffffffffffffff',
+        '0x1234',
+        '0x5678',
+        '0x9abc',
+      ],
+      metadataURI: ['0x1', '0x2', '0x3', '0x4'],
+      salt: '0x7',
+    };
 
     let sig = await signer.signTypedData(domain, proposeTypes, proposeMsg);
     let splitSig = getRSVFromSig(sig);
 
     const proposeCalldata = {
-        r: cairo.uint256(splitSig.r),
-        s: cairo.uint256(splitSig.s),
-        v: splitSig.v,
-        space: proposeMsg.space,
-        author: proposeMsg.author,
-        executionStrategy: proposeMsg.executionStrategy,
-        userProposalValidationParams: proposeMsg.userProposalValidationParams,
-        metadataURI: proposeMsg.metadataURI,
-        salt: cairo.uint256(proposeMsg.salt)
-    }
+      r: cairo.uint256(splitSig.r),
+      s: cairo.uint256(splitSig.s),
+      v: splitSig.v,
+      space: proposeMsg.space,
+      author: proposeMsg.author,
+      executionStrategy: proposeMsg.executionStrategy,
+      userProposalValidationParams: proposeMsg.userProposalValidationParams,
+      metadataURI: proposeMsg.metadataURI,
+      salt: cairo.uint256(proposeMsg.salt),
+    };
 
     let result = await account0.execute({
-        contractAddress: ethSigAuthAddress,
-        entrypoint: "authenticate_propose",
-        calldata: CallData.compile(proposeCalldata as any),
+      contractAddress: ethSigAuthAddress,
+      entrypoint: 'authenticate_propose',
+      calldata: CallData.compile(proposeCalldata as any),
     });
 
     console.log(result);
 
-    // VOTE 
-
-    const voteMsg: Vote = {
-        authenticator: ethSigAuthAddress,
-        space: "0x0000000000000000000000000000000000001234",
-        voter: signer.address,
-        proposalId: "0x1",
-        choice: "0x1",
-        userVotingStrategies: [{ index: '0x0', params: ['0x1', '0x2', '0x3', '0x4'] }],
-        metadataURI: ["0x1", "0x2", "0x3", "0x4"],
-    }
-
-    sig = await signer.signTypedData(domain, voteTypes, voteMsg);
-    splitSig = getRSVFromSig(sig);
-
-    const voteCalldata = {
-        r: cairo.uint256(splitSig.r),
-        s: cairo.uint256(splitSig.s),
-        v: splitSig.v,
-        space: voteMsg.space,
-        voter: voteMsg.voter,
-        proposalId: cairo.uint256(voteMsg.proposalId),
-        choice: voteMsg.choice,
-        userVotingStrategies: voteMsg.userVotingStrategies,
-        metadataURI: voteMsg.metadataURI
-    }
-
-    result = await account0.execute({
-        contractAddress: ethSigAuthAddress,
-        entrypoint: "authenticate_vote",
-        calldata: CallData.compile(voteCalldata as any),
-    });
-
-    console.log(result);
+    // UPDATE PROPOSAL
 
     const updateProposalMsg: UpdateProposal = {
-        authenticator: ethSigAuthAddress,
-        space: "0x0000000000000000000000000000000000001234",
-        author: signer.address,
-        proposalId: "0x1",
-        executionStrategy: {
-            address: "0x0000000000000000000000000000000000005678",
-            params: ["0x0"]
-        },
-        metadataURI: ["0x1", "0x2", "0x3", "0x4"],
-        salt: "0x7"
-    }
+      authenticator: ethSigAuthAddress,
+      space: spaceAddress,
+      author: signer.address,
+      proposalId: '0x1',
+      executionStrategy: {
+        address: '0x0000000000000000000000000000000000005678',
+        params: ['0x0'],
+      },
+      metadataURI: ['0x1', '0x2', '0x3', '0x4'],
+      salt: '0x7',
+    };
 
     sig = await signer.signTypedData(domain, updateProposalTypes, updateProposalMsg);
     splitSig = getRSVFromSig(sig);
 
     const updateProposalCalldata = {
-        r: cairo.uint256(splitSig.r),
-        s: cairo.uint256(splitSig.s),
-        v: splitSig.v,
-        space: updateProposalMsg.space,
-        author: updateProposalMsg.author,
-        proposalId: cairo.uint256(updateProposalMsg.proposalId),
-        executionStrategy: updateProposalMsg.executionStrategy,
-        metadataURI: updateProposalMsg.metadataURI,
-        salt: cairo.uint256(updateProposalMsg.salt)
-    }
+      r: cairo.uint256(splitSig.r),
+      s: cairo.uint256(splitSig.s),
+      v: splitSig.v,
+      space: updateProposalMsg.space,
+      author: updateProposalMsg.author,
+      proposalId: cairo.uint256(updateProposalMsg.proposalId),
+      executionStrategy: updateProposalMsg.executionStrategy,
+      metadataURI: updateProposalMsg.metadataURI,
+      salt: cairo.uint256(updateProposalMsg.salt),
+    };
 
     result = await account0.execute({
-        contractAddress: ethSigAuthAddress,
-        entrypoint: "authenticate_update_proposal",
-        calldata: CallData.compile(updateProposalCalldata as any),
+      contractAddress: ethSigAuthAddress,
+      entrypoint: 'authenticate_update_proposal',
+      calldata: CallData.compile(updateProposalCalldata as any),
     });
 
     console.log(result);
 
+    {
+      // Random Tx just to advance the block number on the devnet so the voting period begins.
 
+      await account0.declareAndDeploy({
+        contract: json.parse(
+          fs
+            .readFileSync('starknet/target/dev/sx_EthSigAuthenticator.sierra.json')
+            .toString('ascii'),
+        ),
+        casm: json.parse(
+          fs.readFileSync('starknet/target/dev/sx_EthSigAuthenticator.casm.json').toString('ascii'),
+        ),
+        constructorCalldata: CallData.compile({}),
+      });
+    }
+
+    // VOTE
+
+    const voteMsg: Vote = {
+      authenticator: ethSigAuthAddress,
+      space: spaceAddress,
+      voter: signer.address,
+      proposalId: '0x1',
+      choice: '0x1',
+      userVotingStrategies: [{ index: '0x0', params: ['0x1', '0x2', '0x3', '0x4'] }],
+      metadataURI: ['0x1', '0x2', '0x3', '0x4'],
+    };
+
+    sig = await signer.signTypedData(domain, voteTypes, voteMsg);
+    splitSig = getRSVFromSig(sig);
+
+    const voteCalldata = {
+      r: cairo.uint256(splitSig.r),
+      s: cairo.uint256(splitSig.s),
+      v: splitSig.v,
+      space: voteMsg.space,
+      voter: voteMsg.voter,
+      proposalId: cairo.uint256(voteMsg.proposalId),
+      choice: voteMsg.choice,
+      userVotingStrategies: voteMsg.userVotingStrategies,
+      metadataURI: voteMsg.metadataURI,
+    };
+
+    result = await account0.execute({
+      contractAddress: ethSigAuthAddress,
+      entrypoint: 'authenticate_vote',
+      calldata: CallData.compile(voteCalldata as any),
+    });
+
+    console.log(result);
   }, 1000000);
 });
 
 function getRSVFromSig(sig: string) {
-    if (sig.startsWith('0x')) {
-      sig = sig.substring(2);
-    }
-    const r = `0x${sig.substring(0, 64)}`;
-    const s = `0x${sig.substring(64, 64 * 2)}`;
-    const v = `0x${sig.substring(64 * 2)}`;
-    return { r, s, v };
+  if (sig.startsWith('0x')) {
+    sig = sig.substring(2);
   }
-
+  const r = `0x${sig.substring(0, 64)}`;
+  const s = `0x${sig.substring(64, 64 * 2)}`;
+  const v = `0x${sig.substring(64 * 2)}`;
+  return { r, s, v };
+}
--- starknet/tests/types.ts
+++ starknet/tests/types.ts
@@ -1,39 +1,39 @@
 export const sharedTypes = {
   Strategy: [
     { name: 'address', type: 'uint256' },
-    { name: 'params', type: 'uint256[]' }
+    { name: 'params', type: 'uint256[]' },
   ],
   IndexedStrategy: [
     { name: 'index', type: 'uint256' },
-    { name: 'params', type: 'uint256[]' }
-  ]
-}
+    { name: 'params', type: 'uint256[]' },
+  ],
+};
 
 export const proposeTypes = {
-    Propose: [
-      { name: 'authenticator', type: 'uint256' },
-      { name: 'space', type: 'uint256' },
-      { name: 'author', type: 'address' },
-      { name: 'executionStrategy', type: 'Strategy' },
-      { name: 'userProposalValidationParams', type: 'uint256[]' },
-      { name: 'metadataURI', type: 'uint256[]'},
-      { name: 'salt', type: 'uint256' }
-    ],
-    Strategy: sharedTypes.Strategy
-  };
+  Propose: [
+    { name: 'authenticator', type: 'uint256' },
+    { name: 'space', type: 'uint256' },
+    { name: 'author', type: 'address' },
+    { name: 'executionStrategy', type: 'Strategy' },
+    { name: 'userProposalValidationParams', type: 'uint256[]' },
+    { name: 'metadataURI', type: 'uint256[]' },
+    { name: 'salt', type: 'uint256' },
+  ],
+  Strategy: sharedTypes.Strategy,
+};
 
 export const voteTypes = {
-    Vote: [
-      { name: 'authenticator', type: 'uint256' },
-      { name: 'space', type: 'uint256' },
-      { name: 'voter', type: 'address' },
-      { name: 'proposalId', type: 'uint256' },
-      { name: 'choice', type: 'uint256' },
-      { name: 'userVotingStrategies', type: 'IndexedStrategy[]' },
-      { name: 'metadataURI', type: 'uint256[]'},
-    ],
-    IndexedStrategy: sharedTypes.IndexedStrategy
-  };
+  Vote: [
+    { name: 'authenticator', type: 'uint256' },
+    { name: 'space', type: 'uint256' },
+    { name: 'voter', type: 'address' },
+    { name: 'proposalId', type: 'uint256' },
+    { name: 'choice', type: 'uint256' },
+    { name: 'userVotingStrategies', type: 'IndexedStrategy[]' },
+    { name: 'metadataURI', type: 'uint256[]' },
+  ],
+  IndexedStrategy: sharedTypes.IndexedStrategy,
+};
 
 export const updateProposalTypes = {
   UpdateProposal: [
@@ -42,31 +42,30 @@ export const updateProposalTypes = {
     { name: 'author', type: 'address' },
     { name: 'proposalId', type: 'uint256' },
     { name: 'executionStrategy', type: 'Strategy' },
-    { name: 'metadataURI', type: 'uint256[]'},
+    { name: 'metadataURI', type: 'uint256[]' },
     { name: 'salt', type: 'uint256' },
   ],
-  Strategy: sharedTypes.Strategy
+  Strategy: sharedTypes.Strategy,
 };
 
-
 export interface Propose {
-    authenticator: string;
-    space: string;
-    author: string;
-    executionStrategy: Strategy;
-    userProposalValidationParams: string[];
-    metadataURI: string[];
-    salt: string;
+  authenticator: string;
+  space: string;
+  author: string;
+  executionStrategy: Strategy;
+  userProposalValidationParams: string[];
+  metadataURI: string[];
+  salt: string;
 }
 
 export interface Vote {
-    authenticator: string;
-    space: string;
-    voter: string;
-    proposalId: string;
-    choice: string;
-    userVotingStrategies: IndexedStrategy[];
-    metadataURI: string[];
+  authenticator: string;
+  space: string;
+  voter: string;
+  proposalId: string;
+  choice: string;
+  userVotingStrategies: IndexedStrategy[];
+  metadataURI: string[];
 }
 
 export interface UpdateProposal {
@@ -79,26 +78,14 @@ export interface UpdateProposal {
   salt: string;
 }
 
-export interface EthereumSigProposeCalldata {
-  r: u256;
-  s: u256;
-  v: number;
-  space: string;
-  author: string;
-  executionStrategy: Strategy;
-  userProposalValidationParams: string[];
-  salt: string;
-}
-
-
 export interface Strategy {
-    address: string;
-    params: string[];
+  address: string;
+  params: string[];
 }
 
 export interface IndexedStrategy {
-    index: string;
-    params: string[];
+  index: string;
+  params: string[];
 }
 
 export interface u256 {
