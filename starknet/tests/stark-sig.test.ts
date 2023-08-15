db4d695b262dd970cdb0021706326a3c464b35e2
Orlando <orlando@snapshot.org>
2023-08-15 21:49:15 +0100

    chore: cleaned up test

--- starknet/tests/stark-sig.test.ts
+++ starknet/tests/stark-sig.test.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import dotenv from 'dotenv';
 import { Provider, Account, CallData, typedData, shortString, json } from 'starknet';
 import {
   proposeTypes,
@@ -12,12 +13,17 @@ import {
   StarknetSigUpdateProposalCalldata,
 } from './types';
 
+dotenv.config();
+
+const network = process.env.NETWORK_URL || '';
+
 describe('Starknet Signature Authenticator', () => {
-  const provider = new Provider({ sequencer: { baseUrl: 'http://127.0.0.1:5050' } });
-  // devnet predeployed account
-  const privateKey0 = '0xe3e70682c2094cac629f6fbed82c07cd';
+  const provider = new Provider({ sequencer: { baseUrl: network } });
+  // starknet devnet predeployed account 0 with seed 0
+
+  const privateKey_0 = '0xe3e70682c2094cac629f6fbed82c07cd';
   const address0 = '0x7e00d496e324876bbc8531f2d9a82bf154d1a04a50218ee74cdd372f75a551a';
-  const account0 = new Account(provider, address0, privateKey0);
+  const account0 = new Account(provider, address0, privateKey_0);
 
   // change this to 'camel' if the account interface uses camel case
   const account0Type = shortString.encodeShortString('snake');
