cd9cda0554b4d1da1612e264c97ca3ab47e44012
Orlando <orlando@snapshot.org>
2023-08-29 15:31:31 +0100

    chore: temp remove deps.sol

--- ethereum/src/deps.sol
+++ ethereum/src/deps.sol
@@ -1,9 +1,9 @@
 /// SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.19;
+// pragma solidity ^0.8.19;
 
-import "@gnosis.pm/safe-contracts/contracts/GnosisSafeL2.sol";
-import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol";
-import "@gnosis.pm/zodiac/contracts/factory/ModuleProxyFactory.sol";
-import "@gnosis.pm/zodiac/contracts/guard/Guardable.sol";
-import "@gnosis.pm/zodiac/contracts/core/Module.sol";
+// import "@gnosis.pm/safe-contracts/contracts/SafeL2.sol";
+// import "@gnosis.pm/safe-contracts/contracts/proxies/SafeProxyFactory.sol";
+// import "@gnosis.pm/zodiac/contracts/factory/ModuleProxyFactory.sol";
+// import "@gnosis.pm/zodiac/contracts/guard/Guardable.sol";
+// import "@gnosis.pm/zodiac/contracts/core/Module.sol";
