e94247eac529900e3df6ccdcce84d30c1606e92a
Orlando <orlando@snapshot.org>
2023-09-15 18:37:09 +0100

    chore: test eth tx auth

---
+++ scripts/eth-tx-auth-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+kill -9 $(lsof -t -i:8545)
+kill -9 $(lsof -t -i:5050)
+yarn chain &
+sleep 10 &&
+yarn hardhat test tests/eth-tx-auth.test.ts --network 'ethereumLocal' --starknet-network 'starknetLocal'
+if [ $? -eq 0 ]
+then
+  kill -9 $(lsof -t -i:8545)
+  kill -9 $(lsof -t -i:5050)
+  exit 0
+else
+  kill -9 $(lsof -t -i:8545)
+  kill -9 $(lsof -t -i:5050)
+  echo "Tests failed"
+  exit 1
+fi
