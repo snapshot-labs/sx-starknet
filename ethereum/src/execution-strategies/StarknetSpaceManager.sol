0770b15ae87f97edd698c7eeb00e5e958e275548
Scott Piriou <30843220+pscott@users.noreply.github.com>
2023-10-09 12:14:45 +0200

    fix: use gaps in SimpleQuorumExecutionStrategy and StarnketSpaceManager

--- ethereum/src/execution-strategies/StarknetSpaceManager.sol
+++ ethereum/src/execution-strategies/StarknetSpaceManager.sol
@@ -57,4 +57,11 @@ contract StarknetSpaceManager is OwnableUpgradeable {
         if (spaces[space] == FALSE) revert InvalidSpace();
         _;
     }
+
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+     uint256[49] private __gap;
 }
