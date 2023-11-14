4df9fb803fceaeaa5766f381a96e49666d24b688
Orlando <orlando@snapshot.org>
2023-11-14 16:41:01 +0000

    refactor: space deployer

--- tests/deploy-space.ts
+++ tests/deploy-space.ts
@@ -19,6 +19,13 @@ async function main() {
   const provider = new RpcProvider({ nodeUrl: network });
   const account = new Account(provider, account_address, account_pk);
 
+  const l1TokenAddress = '0xd96844c9B21CB6cCf2c236257c7fc703E43BA071'; //OZ token 18 decimals
+  const slotIndex = cairo.uint256(0);
+
+  const factsRegistryAddress = '0x57e6ac564b898df3501f5c7d42801ad14d49c2889f29eaec0e0e552fb32ef7f';
+  const timestampsRemapperAddress =
+    '0x047b9a752a0a1fd25c311f6b85c4bafead66a3784b9ccc8d666300d91efca604';
+
   const evmSlotValueVotingStrategySierra = json.parse(
     fs
       .readFileSync('starknet/target/dev/sx_EvmSlotValueVotingStrategy.sierra.json')
@@ -75,24 +82,23 @@ async function main() {
     vanillaProposalValidationStrategyAddress,
   );
 
-  //   const deployResponse = await account.declareAndDeploy({
-  //     contract: evmSlotValueVotingStrategySierra,
-  //     casm: evmSlotValueVotingStrategyCasm,
-  //     constructorCalldata: CallData.compile({
-  //       timestamp_remappers: '0x0231080ccb479ac0fd23a65c83e4be4bb8c93a4e69a6e51e892c529e2f870dfe',
-  //       facts_registry: '0x031653659964bc969905e1b1c18deb69c65efb2e22ff7f4102044d432c91b82d',
-  //     }),
-  //   });
-  const evmSlotValueVotingStrategyAddress =
-    '0x78b96d9c0509e7dc93ae36d17d44b2c8b39453a43e4b563c367993c755fce97'; //deployResponse.deploy.contract_address;
+  // const deployResponse = await account.declareAndDeploy({
+  //   contract: evmSlotValueVotingStrategySierra,
+  //   casm: evmSlotValueVotingStrategyCasm,
+  //   constructorCalldata: CallData.compile({
+  //     timestamp_remappers: timestampsRemapperAddress,
+  //     facts_registry: factsRegistryAddress,
+  //   }),
+  // });
+  const evmSlotValueVotingStrategyAddress = "0x01a46b1377e24153a0de0a22dc72a55c25cc3ead01b3c98f55eef2c3ad206de5" //deployResponse.deploy.contract_address;
   console.log('Voting Strategy Address: ', evmSlotValueVotingStrategyAddress);
 
-//   const spaceDeployResponse = await account.declareAndDeploy({
-//     contract: spaceSierra,
-//     casm: spaceCasm,
-//     constructorCalldata: CallData.compile({}),
-//   });
-  const spaceAddress = "0x038010123867fe31551fbaa813b54d68627034a897e9b1791b801d46de63f127"; //spaceDeployResponse.deploy.contract_address;
+  // const spaceDeployResponse = await account.declareAndDeploy({
+  //   contract: spaceSierra,
+  //   casm: spaceCasm,
+  //   constructorCalldata: CallData.compile({}),
+  // });
+  const spaceAddress = "0x07ad2ef0b2100b37b62c5c132ceefd060c78170c761a56cd0a795ffc4f922fc1" //spaceDeployResponse.deploy.contract_address;
   console.log('Space Address: ', spaceAddress);
 
   // initialize space
@@ -101,9 +107,9 @@ async function main() {
     entrypoint: 'initialize',
     calldata: CallData.compile({
       _owner: 1,
-      _max_voting_duration: 200,
-      _min_voting_duration: 200,
-      _voting_delay: 100,
+      _max_voting_duration: 20000,
+      _min_voting_duration: 20000,
+      _voting_delay: 0,
       _proposal_validation_strategy: {
         address: vanillaProposalValidationStrategyAddress,
         params: [],
@@ -112,7 +118,7 @@ async function main() {
       _voting_strategies: [
         {
           address: evmSlotValueVotingStrategyAddress,
-          params: ['0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984', '0x4'],
+          params: [l1TokenAddress, slotIndex.low, slotIndex.high],
         },
       ],
       _voting_strategies_metadata_uri: [[]],
