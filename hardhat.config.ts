d65c63b8830489ddbd659ee7b61b30b454678583
Scott Piriou <30843220+pscott@users.noreply.github.com>
2024-05-20 15:15:53 +0200

    bump to 0.8.20

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -33,7 +33,7 @@ const config: HardhatUserConfig = {
         },
       },
       {
-        version: '0.8.19',
+        version: '0.8.20',
         settings: {
           optimizer: {
             enabled: true,
