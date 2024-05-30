62f98f86a65b08db0ec4f0c646e65fb4ae43bfb3
Scott Piriou <30843220+pscott@users.noreply.github.com>
2024-05-30 17:15:32 +0200

    forge fmt

--- ethereum/script/Deployer.sol
+++ ethereum/script/Deployer.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.8.19;
 
-import { Script } from "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
 import {L1AvatarExecutionStrategy} from "../src/execution-strategies/L1AvatarExecutionStrategy.sol";
 import {L1AvatarExecutionStrategyFactory} from "../src/execution-strategies/L1AvatarExecutionStrategyFactory.sol";
-import { stdJson } from "forge-std/StdJson.sol";
+import {stdJson} from "forge-std/StdJson.sol";
 
 contract Deployer is Script {
     string internal deployments;
@@ -12,11 +12,9 @@ contract Deployer is Script {
     error ImplementationInitializationFailed();
 
     function run() public {
-
         vm.startBroadcast();
         L1AvatarExecutionStrategy implementation = new L1AvatarExecutionStrategy();
         address owner = vm.addr(vm.envUint("PRIVATE_KEY"));
-
 
         // If the master space is not initialized, initialize it
         address target = address(1);
@@ -26,21 +24,14 @@ contract Deployer is Script {
         if (implementation.owner() == address(0x0)) {
             uint256[] memory starknetSpaces = new uint256[](1);
             starknetSpaces[0] = 4;
-            implementation.setUp(
-                owner,
-                target,
-                starknetCore,
-                executionRelayer,
-                starknetSpaces,
-                quorum
-            );
+            implementation.setUp(owner, target, starknetCore, executionRelayer, starknetSpaces, quorum);
         }
 
-        if (implementation.owner() != owner
-            || implementation.target() != address(0x1)
-            || implementation.starknetCore() != address(2)
-            || implementation.executionRelayer() != 3
-            || implementation.quorum() != 5) {
+        if (
+            implementation.owner() != owner || implementation.target() != address(0x1)
+                || implementation.starknetCore() != address(2) || implementation.executionRelayer() != 3
+                || implementation.quorum() != 5
+        ) {
             // Initialization failed (e.g. got frontran)
             revert ImplementationInitializationFailed();
         }
