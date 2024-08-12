bc96d6dfbb5d1fd78d13830ffc801e73f56f4cad
Scott Piriou <30843220+pscott@users.noreply.github.com>
2024-08-12 14:04:56 +0200

    add invalid signature test

--- tests/stark-sig-auth.test.ts
+++ tests/stark-sig-auth.test.ts
@@ -39,10 +39,10 @@ describe('Starknet Signature Authenticator Tests', function () {
     console.log('account address:', account_address, 'account pk:', account_pk);
 
     const devnetConfig = {
-      args: ["--seed", "42", "--lite-mode"]
+      args: ["--seed", "42", "--lite-mode", "--dump-on", "exit", "--dump-path", "./dump.pkl"],
     };
-    devnet = await Devnet.spawnInstalled(devnetConfig); // TODO
-    console.log(await devnet.provider.isAlive()); // true
+    console.log("Spawning devnet...");
+    devnet = await Devnet.spawnInstalled(devnetConfig); // TODO: should be a neew rather than spawninstalled
 
     provider = new RpcProvider({ nodeUrl: devnet.provider.url });
 
@@ -53,6 +53,7 @@ describe('Starknet Signature Authenticator Tests', function () {
     const domain_version = '0.1.0';
 
     // Deploy the Stark Sig Authenticator
+    console.log("Deploying Stark Sig Authenticator...");
     const { sierraCode: sig_sierra, casmCode: sig_casm } = await getCompiledCode('sx_StarkSigAuthenticator');
     const sig_calldata = CallData.compile({ name: domain_name, version: domain_version });
     const sig_response = await account.declareAndDeploy({
@@ -64,18 +65,21 @@ describe('Starknet Signature Authenticator Tests', function () {
     console.log("Stark Sig Authenticator: ", starkSigAuthenticator.address);
 
     // Deploy the Vanilla Voting strategy
+    console.log("Deploying Voting Strategy...");
     const { sierraCode: voting_sierra, casmCode: voting_casm } = await getCompiledCode('sx_VanillaVotingStrategy');
     const voting_response = await account.declareAndDeploy({ contract: voting_sierra, casm: voting_casm });
     vanillaVotingStrategy = new Contract(voting_sierra.abi, voting_response.deploy.contract_address, provider);
     console.log("Vanilla Voting Strategy: ", vanillaVotingStrategy.address);
 
     // Deploy the Vanilla Proposal Validation strategy
+    console.log("Deploying Validation Strategy...");
     const { sierraCode: proposal_sierra, casmCode: proposal_casm } = await getCompiledCode('sx_VanillaProposalValidationStrategy');
     const proposal_response = await account.declareAndDeploy({ contract: proposal_sierra, casm: proposal_casm });
     vanillaProposalValidationStrategy = new Contract(proposal_sierra.abi, proposal_response.deploy.contract_address, provider);
     console.log("Vanilla Proposal Validation Strategy: ", vanillaProposalValidationStrategy.address);
 
     // Deploy the Space
+    console.log("Deploying Space...");
     const { sierraCode: space_sierra, casmCode: space_casm } = await getCompiledCode('sx_Space');
     const space_response = await account.declareAndDeploy({ contract: space_sierra, casm: space_casm });
     space = new Contract(space_sierra.abi, space_response.deploy.contract_address, provider);
@@ -113,6 +117,7 @@ describe('Starknet Signature Authenticator Tests', function () {
       _metadata_uri,
       _dao_uri);
     await provider.waitForTransaction(initializeRes.transaction_hash);
+    console.log("Space initialized");
 
     domain = {
       name: domain_name,
@@ -120,14 +125,19 @@ describe('Starknet Signature Authenticator Tests', function () {
       chainId: '0x534e5f5345504f4c4941', // 'SN_SEPOLIA'
     };
 
-    // TODO: Dumping the Starknet state so it can be loaded at the same point for each test
-    // await starknet.devnet.dump('dump.pkl');
+    // Dumping the Starknet state so it can be loaded at the same point for each test
+    console.log("Dumping state...");
+    await devnet.provider.dump('dump.pkl');
+    console.log("State dumped");
   });
 
   it('can authenticate a proposal, a vote, and a proposal update', async () => {
-    // await starknet.devnet.restart();
-    // await starknet.devnet.load('./dump.pkl');
-    // await starknet.devnet.increaseTime(10);
+    console.log("Restarting devnet...");
+    await devnet.provider.restart();
+    console.log("Loading state...");
+    await devnet.provider.load('./dump.pkl');
+    console.log("Increasing time by 10");
+    await devnet.provider.increaseTime(10); // TODO check if necessary
 
     // PROPOSE
     const proposeMsg: Propose = {
@@ -155,17 +165,10 @@ describe('Starknet Signature Authenticator Tests', function () {
       message: proposeMsg as any,
     } as StarknetTypedData)) as any;
 
-    const d = {
-      types: proposeTypes,
-      primaryType: 'Propose',
-      domain: domain,
-      message: proposeMsg as any,
-    } as StarknetTypedData;
-
-    starkSigAuthenticator.connect(account);
 
     console.log("Authenticating proposal...");
     const proposeSignature = [proposeSig.r, proposeSig.s];
+    starkSigAuthenticator.connect(account);
     const proposeRes = await starkSigAuthenticator.authenticate_propose(proposeSignature, proposeMsg.space, proposeMsg.author, proposeMsg.metadataUri, proposeMsg.executionStrategy, proposeMsg.userProposalValidationParams, proposeMsg.salt);
     await provider.waitForTransaction(proposeRes.transaction_hash);
 
@@ -222,185 +225,164 @@ describe('Starknet Signature Authenticator Tests', function () {
     await provider.waitForTransaction(voteRes.transaction_hash);
   });
 
-  // it('should revert if an incorrect signature is used', async () => {
-  //   await starknet.devnet.restart();
-  //   await starknet.devnet.load('./dump.pkl');
-  //   await starknet.devnet.increaseTime(10);
+  it('should revert if an incorrect signature is used', async () => {
+    await devnet.provider.restart();
+    await devnet.provider.load('./dump.pkl');
+    await devnet.provider.increaseTime(10); // TODO check if necessary
 
-  //   // Account #1 on Starknet devnet with seed 42
-  //   const invalidAccountWithSigner = new Account(
-  //     new Provider({ sequencer: { baseUrl: network } }),
-  //     '0x7aac39162d91acf2c4f0d539f4b81e23832619ac0c3df9fce22e4a8d505632a',
-  //     '0x23b8c1e9392456de3eb13b9046685257',
-  //   );
+    // Account #1 on Starknet devnet with seed 42
+    const invalidAccount = new Account(
+      provider,
+      '0x7aac39162d91acf2c4f0d539f4b81e23832619ac0c3df9fce22e4a8d505632a',
+      '0x23b8c1e9392456de3eb13b9046685257',
+    );
 
-  //   // PROPOSE
-  //   const proposeMsg: Propose = {
-  //     space: space.address,
-  //     author: accountWithSigner.address,
-  //     metadataUri: ['0x1', '0x2', '0x3', '0x4'],
-  //     executionStrategy: {
-  //       address: '0x0000000000000000000000000000000000005678',
-  //       params: ['0x0'],
-  //     },
-  //     userProposalValidationParams: [
-  //       '0xffffffffffffffffffffffffffffffffffffffffff',
-  //       '0x1234',
-  //       '0x5678',
-  //       '0x9abc',
-  //     ],
-  //     salt: '0x0',
-  //   };
+    // PROPOSE
+    const proposeMsg: Propose = {
+      space: space.address,
+      author: account.address,
+      metadataUri: ['0x1', '0x2', '0x3', '0x4'],
+      executionStrategy: {
+        address: '0x0000000000000000000000000000000000005678',
+        params: ['0x0'],
+      },
+      userProposalValidationParams: [
+        '0xffffffffffffffffffffffffffffffffffffffffff',
+        '0x1234',
+        '0x5678',
+        '0x9abc',
+      ],
+      salt: '0x0',
+    };
 
-  //   const invalidProposeSig = (await invalidAccountWithSigner.signMessage({
-  //     types: proposeTypes,
-  //     primaryType: 'Propose',
-  //     domain: domain,
-  //     message: proposeMsg as any,
-  //   } as typedData.TypedData)) as any;
+    const invalidProposeSignature = (await invalidAccount.signMessage({
+      types: proposeTypes,
+      primaryType: 'Propose',
+      domain: domain,
+      message: proposeMsg as any,
+    } as StarknetTypedData)) as any;
 
-  //   const invalidProposeCalldata = CallData.compile({
-  //     signature: [invalidProposeSig.r, invalidProposeSig.s],
-  //     ...proposeMsg,
-  //   });
+    starkSigAuthenticator.connect(account);
 
-  //   try {
-  //     await account.invoke(starkSigAuthenticator, 'authenticate_propose', invalidProposeCalldata, {
-  //       rawInput: true,
-  //     });
-  //     expect.fail('Should have failed');
-  //   } catch (err: any) {
-  //     expect(err.message).to.contain(shortString.encodeShortString('Invalid Signature'));
-  //   }
+    console.log("Authenticating invalid proposal...");
 
-  //   const proposeSig = (await accountWithSigner.signMessage({
-  //     types: proposeTypes,
-  //     primaryType: 'Propose',
-  //     domain: domain,
-  //     message: proposeMsg as any,
-  //   } as typedData.TypedData)) as any;
+    const invalidProposeSig = [invalidProposeSignature.r, invalidProposeSignature.s];
 
-  //   const proposeCalldata = CallData.compile({
-  //     signature: [proposeSig.r, proposeSig.s],
-  //     ...proposeMsg,
-  //   });
+    try {
+      const invalidProposeRes = await starkSigAuthenticator.authenticate_propose(invalidProposeSig, proposeMsg.space, proposeMsg.author, proposeMsg.metadataUri, proposeMsg.executionStrategy, proposeMsg.userProposalValidationParams, proposeMsg.salt);
+      await provider.waitForTransaction(invalidProposeRes.transaction_hash);
+      expect.fail('Should have failed');
+    } catch (err: any) {
+      expect(err.message).to.contain(shortString.encodeShortString('Invalid Signature'));
+    }
 
-  //   await account.invoke(starkSigAuthenticator, 'authenticate_propose', proposeCalldata, {
-  //     rawInput: true,
-  //   });
+    const proposeSignature = (await account.signMessage({
+      types: proposeTypes,
+      primaryType: 'Propose',
+      domain: domain,
+      message: proposeMsg as any,
+    } as StarknetTypedData)) as any;
 
-  //   // UPDATE PROPOSAL
+    const proposeCalldata = CallData.compile({
+      signature: [proposeSignature.r, proposeSignature.s],
+      ...proposeMsg,
+    });
 
-  //   const updateProposalMsg: UpdateProposal = {
-  //     space: space.address,
-  //     author: accountWithSigner.address,
-  //     proposalId: { low: '0x1', high: '0x0' },
-  //     executionStrategy: {
-  //       address: '0x0000000000000000000000000000000000005678',
-  //       params: ['0x5', '0x6', '0x7', '0x8'],
-  //     },
-  //     metadataUri: ['0x1', '0x2', '0x3', '0x4'],
-  //     salt: '0x1',
-  //   };
+    // Should not fail this time !
+    console.log("Authenticating proposal...");
+    const proposeSig = [proposeSignature.r, proposeSignature.s];
+    const proposeRes = await starkSigAuthenticator.authenticate_propose(proposeSig, proposeMsg.space, proposeMsg.author, proposeMsg.metadataUri, proposeMsg.executionStrategy, proposeMsg.userProposalValidationParams, proposeMsg.salt);
+    await provider.waitForTransaction(proposeRes.transaction_hash);
 
-  //   const invalidUpdateProposalSig = (await invalidAccountWithSigner.signMessage({
-  //     types: updateProposalTypes,
-  //     primaryType: 'UpdateProposal',
-  //     domain: domain,
-  //     message: updateProposalMsg as any,
-  //   } as typedData.TypedData)) as any;
 
-  //   const invalidUpdateProposalCalldata = CallData.compile({
-  //     signature: [invalidUpdateProposalSig.r, invalidUpdateProposalSig.s],
-  //     ...updateProposalMsg,
-  //   });
+    // UPDATE PROPOSAL
 
-  //   try {
-  //     await account.invoke(
-  //       starkSigAuthenticator,
-  //       'authenticate_update_proposal',
-  //       invalidUpdateProposalCalldata,
-  //       {
-  //         rawInput: true,
-  //       },
-  //     );
-  //     expect.fail('Should have failed');
-  //   } catch (err: any) {
-  //     expect(err.message).to.contain(shortString.encodeShortString('Invalid Signature'));
-  //   }
+    const updateProposalMsg: UpdateProposal = {
+      space: space.address,
+      author: account.address,
+      proposalId: { low: '0x1', high: '0x0' },
+      executionStrategy: {
+        address: '0x0000000000000000000000000000000000005678',
+        params: ['0x5', '0x6', '0x7', '0x8'],
+      },
+      metadataUri: ['0x1', '0x2', '0x3', '0x4'],
+      salt: '0x1',
+    };
 
-  //   const updateProposalSig = (await accountWithSigner.signMessage({
-  //     types: updateProposalTypes,
-  //     primaryType: 'UpdateProposal',
-  //     domain: domain,
-  //     message: updateProposalMsg as any,
-  //   } as typedData.TypedData)) as any;
+    const invalidUpdateProposalSignature = (await invalidAccount.signMessage({
+      types: updateProposalTypes,
+      primaryType: 'UpdateProposal',
+      domain: domain,
+      message: updateProposalMsg as any,
+    } as StarknetTypedData)) as any;
 
-  //   const updateProposalCalldata = CallData.compile({
-  //     signature: [updateProposalSig.r, updateProposalSig.s],
-  //     ...updateProposalMsg,
-  //   });
+    console.log("Authenticating invalid update proposal...");
 
-  //   await account.invoke(
-  //     starkSigAuthenticator,
-  //     'authenticate_update_proposal',
-  //     updateProposalCalldata,
-  //     {
-  //       rawInput: true,
-  //     },
-  //   );
+    const invalidUpdateProposalSig = [invalidUpdateProposalSignature.r, invalidUpdateProposalSignature.s];
 
-  //   // Increase time so voting period begins
-  //   await starknet.devnet.increaseTime(100);
+    try {
+      const invalidUpdateProposalRes = await starkSigAuthenticator.authenticate_update_proposal(invalidUpdateProposalSig, updateProposalMsg.space, updateProposalMsg.author, updateProposalMsg.proposalId, updateProposalMsg.executionStrategy, updateProposalMsg.metadataUri, updateProposalMsg.salt);
+      await provider.waitForTransaction(invalidUpdateProposalRes.transaction_hash);
+      expect.fail('Should have failed');
+    } catch (err: any) {
+      expect(err.message).to.contain(shortString.encodeShortString('Invalid Signature'));
+    }
 
-  //   // VOTE
+    const updateProposalSignature = (await account.signMessage({
+      types: updateProposalTypes,
+      primaryType: 'UpdateProposal',
+      domain: domain,
+      message: updateProposalMsg as any,
+    } as StarknetTypedData)) as any;
 
-  //   const voteMsg: Vote = {
-  //     space: space.address,
-  //     voter: accountWithSigner.address,
-  //     proposalId: { low: '0x1', high: '0x0' },
-  //     choice: '0x1',
-  //     userVotingStrategies: [{ index: '0x0', params: ['0x1', '0x2', '0x3', '0x4'] }],
-  //     metadataUri: ['0x1', '0x2', '0x3', '0x4'],
-  //   };
+    const updateProposalSig = [updateProposalSignature.r, updateProposalSignature.s];
+    const updateProposalRes = await starkSigAuthenticator.authenticate_update_proposal(updateProposalSig, updateProposalMsg.space, updateProposalMsg.author, updateProposalMsg.proposalId, updateProposalMsg.executionStrategy, updateProposalMsg.metadataUri, updateProposalMsg.salt);
+    await provider.waitForTransaction(updateProposalRes.transaction_hash);
 
-  //   const invalidVoteSig = (await invalidAccountWithSigner.signMessage({
-  //     types: voteTypes,
-  //     primaryType: 'Vote',
-  //     domain: domain,
-  //     message: voteMsg as any,
-  //   } as typedData.TypedData)) as any;
+    // Increase time so voting period begins
+    await devnet.provider.increaseTime(100);
 
-  //   const invalidVoteCalldata = CallData.compile({
-  //     signature: [invalidVoteSig.r, invalidVoteSig.s],
-  //     ...voteMsg,
-  //   });
+    // VOTE
 
-  //   try {
-  //     await account.invoke(starkSigAuthenticator, 'authenticate_vote', invalidVoteCalldata, {
-  //       rawInput: true,
-  //     });
-  //     expect.fail('Should have failed');
-  //   } catch (err: any) {
-  //     expect(err.message).to.contain(shortString.encodeShortString('Invalid Signature'));
-  //   }
+    console.log("Casting invalid vote...");
+    const voteMsg: Vote = {
+      space: space.address,
+      voter: account.address,
+      proposalId: { low: '0x1', high: '0x0' },
+      choice: '0x1',
+      userVotingStrategies: [{ index: '0x0', params: ['0x1', '0x2', '0x3', '0x4'] }],
+      metadataUri: ['0x1', '0x2', '0x3', '0x4'],
+    };
 
-  //   const voteSig = (await accountWithSigner.signMessage({
-  //     types: voteTypes,
-  //     primaryType: 'Vote',
-  //     domain: domain,
-  //     message: voteMsg as any,
-  //   } as typedData.TypedData)) as any;
+    const invalidVoteSignature = (await invalidAccount.signMessage({
+      types: voteTypes,
+      primaryType: 'Vote',
+      domain: domain,
+      message: voteMsg as any,
+    } as StarknetTypedData)) as any;
 
-  //   const voteCalldata = CallData.compile({
-  //     signature: [voteSig.r, voteSig.s],
-  //     ...voteMsg,
-  //   });
+    const invalidVoteSig = [invalidVoteSignature.r, invalidVoteSignature.s];
 
-  //   await account.invoke(starkSigAuthenticator, 'authenticate_vote', voteCalldata, {
-  //     rawInput: true,
-  //   });
-  // }, 1000000);
+    const choice = new CairoCustomEnum({ For: {} });
+    try {
+      const invalidVoteRes = await starkSigAuthenticator.authenticate_vote(invalidVoteSig, voteMsg.space, voteMsg.voter, voteMsg.proposalId, choice, voteMsg.userVotingStrategies, voteMsg.metadataUri);
+      await provider.waitForTransaction(invalidVoteRes.transaction_hash);
+      expect.fail('Should have failed');
+    } catch (err: any) {
+      expect(err.message).to.contain(shortString.encodeShortString('Invalid Signature'));
+    }
+
+    const voteSignature = (await account.signMessage({
+      types: voteTypes,
+      primaryType: 'Vote',
+      domain: domain,
+      message: voteMsg as any,
+    } as StarknetTypedData)) as any;
+
+    const voteSig = [voteSignature.r, voteSignature.s];
+    const voteRes = await starkSigAuthenticator.authenticate_vote(voteSig, voteMsg.space, voteMsg.voter, voteMsg.proposalId, choice, voteMsg.userVotingStrategies, voteMsg.metadataUri);
+    await provider.waitForTransaction(voteRes.transaction_hash);
+  });
 
   // it('should revert if a salt is reused by an author when creating or updating a proposal', async () => {
   //   await starknet.devnet.restart();
