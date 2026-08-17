21cd066c9782a5c10d1302a435248762cba1845a
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2026-08-17 15:26:31 +0200

    chore: migrate Ethereum libraries to npm

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,1 @@
-import config from '@snapshot-labs/eslint-config';
-
-export default [
-  ...config,
-  // Foundry dependencies: they ship their own prettier configs which pull in
-  // plugins that are not installed here.
-  { ignores: ['ethereum/lib/**'] }
-];
+export { default } from '@snapshot-labs/eslint-config';
